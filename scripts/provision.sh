9776c60dd7335b405eb8b31df7df1fe9b8211cbd
Joe Ferguson <joe@joeferguson.me>
2017-11-30 13:06:45 +1100

    ❄️ 💎 🔖 🚀 🎨 ⚡️ v5.0.0 Drop all manual build proceses in favor of utilizing Packer (#129)
    Instead of having to worry about installing guest extensions for each provider we can leverage the work done on the [chef/bento](https://github.com/chef/bento) repo and utilize [Packer](https://www.packer.io/) to build the Homestead box.
    
    This allows us to ship the *most up to date* Ubuntu box without having to rely on the release schedule of chef/bento base boxes that we have been uusing.
    
    (cherry picked from commit 1f58841)

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -236,10 +236,3 @@ sed -i "s/#START=yes/START=yes/" /etc/default/beanstalkd
 /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
 /sbin/mkswap /var/swap.1
 /sbin/swapon /var/swap.1
-
-# Minimize The Disk Image
-
-echo "Minimizing disk image..."
-dd if=/dev/zero of=/EMPTY bs=1M
-rm -f /EMPTY
-sync
