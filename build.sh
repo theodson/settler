21fa744eff6589f8fe8e959814dc6f439ab2a9aa
Theodson <theodson@gmail.com>
2018-08-09 12:18:47 +1000

    fix kickstart partitioning --nohome issue using explcit part commands
    fix kickstart partitioning --nohome issue using explcit part commands

--- build.sh
+++ build.sh
@@ -11,7 +11,8 @@ git clone https://github.com/chef/bento.git 2>/dev/null || echo 'bento/bento dir
 
 packer_options=' --on-error=abort '
 
-packer_vars=" -var name=homestead-co7 -var memory=2048 -var disk_size=105000 -var cpus=2 -var box_basename=homestead-co7 -var version=$PACKER_BOX_VERSION "
+#packer_vars=" -var name=homestead-co7 -var memory=2048 -var disk_size=105000 -var cpus=2 -var box_basename=homestead-co7 -var version=$PACKER_BOX_VERSION "
+packer_vars=" -var name=homestead-co7 -var memory=2048 -var disk_size=105000 -var cpus=2 -var version=$PACKER_BOX_VERSION "
 
 rm -f scripts/homestead.sh &> /dev/null
 cp -rf scripts/provision.sh bento/centos/scripts/homestead.sh
@@ -24,20 +25,25 @@ grep 'homestead.sh' centos-7.5-x86_64.json &> /dev/null || (
     ex -sc "${lineno}i|\"scripts/homestead.sh\"," -cx centos-7.5-x86_64.json )
 
 # Ensure simple partitioning
-grep 'autopart --nohome' http/7/ks.cfg &> /dev/null || (
-    lineno=$(grep -n '^autopart' http/7/ks.cfg | cut -d: -f1) && \
+lineno=$(grep -n '^autopart' http/7/ks.cfg | cut -d: -f1)
+if [ ! -z $lineno ]; then
+    # https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/7/html/installation_guide/sect-kickstart-syntax
+    # Disk partitioning information
     echo "Attempting insert of simple auto partition for kickstart file at ${lineno}" && \
-    ex -sc "${lineno}d|${lineno}i|autopart --nohome" -cx http/7/ks.cfg )
-
+    ex -sc "${lineno}d" -cx http/7/ks.cfg 
+    ex -sc "${lineno}i|part /boot --fstype=\"xfs\" --size=1024" -cx http/7/ks.cfg 
+    ex -sc "$((++lineno))i|part pv.01 --size 1 --grow" -cx http/7/ks.cfg 
+    ex -sc "$((++lineno))i|volgroup centos pv.01" -cx http/7/ks.cfg 
+    ex -sc "$((++lineno))i|logvol / --fstype=\"xfs\" --size=4096 --grow --vgname=centos --name=lv_root" -cx http/7/ks.cfg 
+    ex -sc "$((++lineno))i|logvol swap --size=8192 --vgname=centos --name=lv_swap" -cx http/7/ks.cfg 
+    ex -sc "$((++lineno))i|logvol /tmp --fstype=\"xfs\" --size=1024 --vgname=centos --name=lv_tmp" -cx http/7/ks.cfg 
+fi
 
 # Add VERSIONING information into homestead.sh
 grep 'PACKER_BOX_VERSION=' scripts/homestead.sh &> /dev/null || (
     lineno=4 && \
     echo "Attempting insert of PACKER_BOX_VERSION into homestead settler script at ${lineno}" && \
     ex -sc "${lineno}i|PACKER_BOX_VERSION=${PACKER_BOX_VERSION=}" -cx scripts/homestead.sh )
-
-
-
 
 
 echo packer build ${packer_options} ${packer_vars} centos-7.5-x86_64.json
