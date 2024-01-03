545eb076cacd73115b098bb00cdba44f6cf4c00e
Theo <theodson@gmail.com>
2024-01-04 10:44:09 +1100

    Fix sed commands for linux in bin/use_homestead_features.sh

--- bin/use_homestead_features.sh
+++ bin/use_homestead_features.sh
@@ -6,12 +6,12 @@
 #
 if true; then
   # use 20.04.6
-  sed -i '' 's/ubuntu-20.04.5-live-server-amd64.iso/ubuntu-20.04.6-live-server-amd64.iso/' ../bento/packer_templates/ubuntu/ubuntu-20.04-amd64.json
-  sed -i '' 's/sha256:5035be37a7e9abbdc09f0d257f3e33416c1a0fb322ba860d42d74aa75c3468d4/sha256:b8f31413336b9393ad5d8ef0282717b2ab19f007df2e9ed5196c13d8f9153c8b/' ../bento/packer_templates/ubuntu/ubuntu-20.04-amd64.json
+  sed -i 's/ubuntu-20.04.5-live-server-amd64.iso/ubuntu-20.04.6-live-server-amd64.iso/' ../bento/packer_templates/ubuntu/ubuntu-20.04-amd64.json
+  sed -i 's/sha256:5035be37a7e9abbdc09f0d257f3e33416c1a0fb322ba860d42d74aa75c3468d4/sha256:b8f31413336b9393ad5d8ef0282717b2ab19f007df2e9ed5196c13d8f9153c8b/' ../bento/packer_templates/ubuntu/ubuntu-20.04-amd64.json
 else
   # use 20.04.5
-  sed -i '' 's/releases.ubuntu.com/old-releases.ubuntu.com/' ../bento/packer_templates/ubuntu/ubuntu-20.04-amd64.json
-  sed -i '' 's/focal/releases\/20.04.5/' ../bento/packer_templates/ubuntu/ubuntu-20.04-amd64.json
+  sed -i 's/releases.ubuntu.com/old-releases.ubuntu.com/' ../bento/packer_templates/ubuntu/ubuntu-20.04-amd64.json
+  sed -i 's/focal/releases\/20.04.5/' ../bento/packer_templates/ubuntu/ubuntu-20.04-amd64.json
 fi
 
 #
@@ -32,7 +32,7 @@ cat << COPY_FEATURE_FOLDER > "scripts/amd64.features-upload"
             "type": "file"
         },
 COPY_FEATURE_FOLDER
-sed -i '' -e "${insertline}r scripts/amd64.features-upload" ../bento/packer_templates/ubuntu/ubuntu-20.04-amd64.json
+sed -i -e "${insertline}r scripts/amd64.features-upload" ../bento/packer_templates/ubuntu/ubuntu-20.04-amd64.json
 
 insertline=$(echo "$(grep -n '# One last upgrade check' scripts/amd64.sh | cut -d : -f 1) -1" | bc)
 echo > scripts/amd64.features
@@ -42,9 +42,9 @@ for feature in golang rustc rabbitmq minio mailpit python pm2 meilisearch ohmyzs
   cat  ../homestead/scripts/features/${feature}.sh >> scripts/amd64.features
 done
 echo "# ===========================  FEATURES END  ============================" >> scripts/amd64.features
-sed -i '' -e '/usr\/bin\/env bash/d' scripts/amd64.features
-sed -i '' -e "s/exit 0/echo 'skipping exit 0'/g" scripts/amd64.features
-sed -i '' -e "${insertline}r scripts/amd64.features" scripts/amd64.sh
+sed -i -e '/usr\/bin\/env bash/d' scripts/amd64.features
+sed -i -e "s/exit 0/echo 'skipping exit 0'/g" scripts/amd64.features
+sed -i -e "${insertline}r scripts/amd64.features" scripts/amd64.sh
 
 
 #
@@ -65,7 +65,7 @@ cat << COPY_FEATURE_FOLDER > "scripts/arm.features-upload"
             "type": "file"
         },
 COPY_FEATURE_FOLDER
-sed -i '' -e "${insertline}r scripts/arm.features-upload" ../bento/packer_templates/ubuntu/ubuntu-20.04-arm64.json
+sed -i -e "${insertline}r scripts/arm.features-upload" ../bento/packer_templates/ubuntu/ubuntu-20.04-arm64.json
 
 insertlinearm=$(echo "$(grep -n '# One last upgrade check' scripts/arm.sh | cut -d : -f 1) -1" | bc)
 echo > scripts/arm.features
@@ -75,9 +75,9 @@ for feature in golang rustc rabbitmq minio mailpit python pm2 meilisearch ohmyzs
   cat  ../homestead/scripts/features/${feature}.sh >> scripts/arm.features
 done
 echo "# ===========================  FEATURES END  ============================" >> scripts/arm.features
-sed -i '' -e '/usr\/bin\/env bash/d' scripts/arm.features
-sed -i '' -e "s/exit 0/echo 'skipping exit 0'/g" scripts/arm.features
-sed -i '' -e "${insertlinearm}r scripts/arm.features" scripts/arm.sh
+sed -i -e '/usr\/bin\/env bash/d' scripts/arm.features
+sed -i -e "s/exit 0/echo 'skipping exit 0'/g" scripts/arm.features
+sed -i -e "${insertlinearm}r scripts/arm.features" scripts/arm.sh
 
 /bin/ln -f scripts/amd64.sh ../bento/packer_templates/ubuntu/scripts/homestead.sh
 /bin/ln -f scripts/arm.sh ../bento/packer_templates/ubuntu/scripts/homestead-arm.sh
