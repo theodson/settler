f19875cc9aabe69c74bbf617615dbf5dbae33b19
Theo <theodson@gmail.com>
2024-01-06 11:57:21 +1100

    template path to var

--- bin/use-homestead-features.sh
+++ bin/use-homestead-features.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 
+packer_template_amd64=../bento/packer_templates/ubuntu/ubuntu-20.04-amd64.json
+packer_template_arm64=../bento/packer_templates/ubuntu/ubuntu-20.04-arm64.json
 #
 # Temporary fix for failing ISO ubuntu-20.04.5 until ubuntu-20.04.6-live-server-amd64 is available in bento. 
 # 20.04.6 is released but bento has not yet updated - must refer to old-releases site for 20.04.5 
 #
 if true; then
   # use 20.04.6
-  sed -i 's/ubuntu-20.04.5-live-server-amd64.iso/ubuntu-20.04.6-live-server-amd64.iso/' ../bento/packer_templates/ubuntu/ubuntu-20.04-amd64.json
-  sed -i 's/sha256:5035be37a7e9abbdc09f0d257f3e33416c1a0fb322ba860d42d74aa75c3468d4/sha256:b8f31413336b9393ad5d8ef0282717b2ab19f007df2e9ed5196c13d8f9153c8b/' ../bento/packer_templates/ubuntu/ubuntu-20.04-amd64.json
+  sed -i 's/ubuntu-20.04.5-live-server-amd64.iso/ubuntu-20.04.6-live-server-amd64.iso/' $packer_template_amd64
+  sed -i 's/sha256:5035be37a7e9abbdc09f0d257f3e33416c1a0fb322ba860d42d74aa75c3468d4/sha256:b8f31413336b9393ad5d8ef0282717b2ab19f007df2e9ed5196c13d8f9153c8b/' $packer_template_amd64
 else
   # use 20.04.5
-  sed -i 's/releases.ubuntu.com/old-releases.ubuntu.com/' ../bento/packer_templates/ubuntu/ubuntu-20.04-amd64.json
-  sed -i 's/focal/releases\/20.04.5/' ../bento/packer_templates/ubuntu/ubuntu-20.04-amd64.json
+  sed -i 's/releases.ubuntu.com/old-releases.ubuntu.com/' $packer_template_amd64
+  sed -i 's/focal/releases\/20.04.5/' $packer_template_amd64
 fi
 
 #
 # amd64
 #
-insertline=$(echo "$(grep -n '"provisioners":' ../bento/packer_templates/ubuntu/ubuntu-20.04-amd64.json | cut -d : -f 1)" | bc)
+insertline=$(echo "$(grep -n '"provisioners":' $packer_template_amd64 | cut -d : -f 1)" | bc)
 cat << COPY_FEATURE_FOLDER > "scripts/amd64.features-upload"
         {
             "inline": [
@@ -37,7 +39,7 @@ cat << COPY_FEATURE_FOLDER > "scripts/amd64.features-upload"
             "type": "file"
         },
 COPY_FEATURE_FOLDER
-sed -i -e "${insertline}r scripts/amd64.features-upload" ../bento/packer_templates/ubuntu/ubuntu-20.04-amd64.json
+sed -i -e "${insertline}r scripts/amd64.features-upload" $packer_template_amd64
 
 insertline=$(echo "$(grep -n '# One last upgrade check' scripts/amd64.sh | cut -d : -f 1) -1" | bc)
 echo > scripts/amd64.features
@@ -62,7 +64,7 @@ sed -i -e "${insertline}r scripts/amd64.features" scripts/amd64.sh
 #
 # ARM
 #
-insertline=$(echo "$(grep -n '"provisioners":' ../bento/packer_templates/ubuntu/ubuntu-20.04-arm64.json | cut -d : -f 1)" | bc)
+insertline=$(echo "$(grep -n '"provisioners":' $packer_template_arm64 | cut -d : -f 1)" | bc)
 cat << COPY_FEATURE_FOLDER > "scripts/arm.features-upload"
         {
             "inline": [
@@ -82,7 +84,7 @@ cat << COPY_FEATURE_FOLDER > "scripts/arm.features-upload"
             "type": "file"
         },
 COPY_FEATURE_FOLDER
-sed -i -e "${insertline}r scripts/arm.features-upload" ../bento/packer_templates/ubuntu/ubuntu-20.04-arm64.json
+sed -i -e "${insertline}r scripts/arm.features-upload" $packer_template_arm64
 
 insertlinearm=$(echo "$(grep -n '# One last upgrade check' scripts/arm.sh | cut -d : -f 1) -1" | bc)
 echo > scripts/arm.features
