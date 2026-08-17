f8cf543c51709b4a074385cbc1f8882bd33d933b
Theo <theodson@gmail.com>
2026-08-18 07:40:20 +1000

    Add PHP 8.5 support and upgrade PostgreSQL to 15
    This change introduces the installation and configuration of PHP 8.5, including common extensions, CLI and FPM settings, Xdebug, and OPcache. It also updates the default PostgreSQL installation from version 14 to 15.

--- scripts/amd64.sh
+++ scripts/amd64.sh
@@ -517,7 +517,51 @@ else
 
   touch /home/vagrant/.homestead-features/php83
 
-  # Disable old PHP FPM
+  # PHP 8.5
+  apt-get install -y --allow-change-held-packages \
+    php8.5 php8.5-bcmath php8.5-bz2 php8.5-cgi php8.5-cli php8.5-common php8.5-curl php8.5-dba php8.5-dev \
+    php8.5-enchant php8.5-fpm php8.5-gd php8.5-gmp php8.5-imap php8.5-interbase php8.5-intl php8.5-ldap \
+    php8.5-mbstring php8.5-mysql php8.5-odbc php8.5-pgsql php8.5-phpdbg php8.5-pspell php8.5-readline \
+    php8.5-snmp php8.5-soap php8.5-sqlite3 php8.5-sybase php8.5-tidy php8.5-xml php8.5-xsl \
+    php8.5-zip php8.5-imagick php8.5-memcached php8.5-redis php8.5-xmlrpc php8.5-xdebug
+
+  # Configure php.ini for CLI
+  sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/8.5/cli/php.ini
+  sed -i "s/display_errors = .*/display_errors = On/" /etc/php/8.5/cli/php.ini
+  sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/8.5/cli/php.ini
+  sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/8.5/cli/php.ini
+
+  # Configure Xdebug
+  echo "xdebug.mode = debug" >> /etc/php/8.5/mods-available/xdebug.ini
+  echo "xdebug.discover_client_host = true" >> /etc/php/8.5/mods-available/xdebug.ini
+  echo "xdebug.client_port = 9003" >> /etc/php/8.5/mods-available/xdebug.ini
+  echo "xdebug.max_nesting_level = 512" >> /etc/php/8.5/mods-available/xdebug.ini
+  echo "opcache.revalidate_freq = 0" >> /etc/php/8.5/mods-available/opcache.ini
+
+  # Configure php.ini for FPM
+  sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/8.5/fpm/php.ini
+  sed -i "s/display_errors = .*/display_errors = On/" /etc/php/8.5/fpm/php.ini
+  sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/8.5/fpm/php.ini
+  sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/8.5/fpm/php.ini
+  sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php/8.5/fpm/php.ini
+  sed -i "s/post_max_size = .*/post_max_size = 100M/" /etc/php/8.5/fpm/php.ini
+  sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/8.5/fpm/php.ini
+
+  printf "[openssl]\n" | tee -a /etc/php/8.5/fpm/php.ini
+  printf "openssl.cainfo = /etc/ssl/certs/ca-certificates.crt\n" | tee -a /etc/php/8.5/fpm/php.ini
+  printf "[curl]\n" | tee -a /etc/php/8.5/fpm/php.ini
+  printf "curl.cainfo = /etc/ssl/certs/ca-certificates.crt\n" | tee -a /etc/php/8.5/fpm/php.ini
+
+  # Configure FPM
+  sed -i "s/user = www-data/user = vagrant/" /etc/php/8.5/fpm/pool.d/www.conf
+  sed -i "s/group = www-data/group = vagrant/" /etc/php/8.5/fpm/pool.d/www.conf
+  sed -i "s/listen\.owner.*/listen.owner = vagrant/" /etc/php/8.5/fpm/pool.d/www.conf
+  sed -i "s/listen\.group.*/listen.group = vagrant/" /etc/php/8.5/fpm/pool.d/www.conf
+  sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/8.5/fpm/pool.d/www.conf
+
+  touch /home/vagrant/.homestead-features/php85
+  
+    # Disable old PHP FPM
   systemctl disable php5.6-fpm
   systemctl disable php7.0-fpm
   systemctl disable php7.1-fpm
@@ -527,10 +571,14 @@ else
   systemctl disable php8.0-fpm
   systemctl disable php8.1-fpm
   systemctl disable php8.2-fpm
+  systemctl disable php8.3-fpm
+  systemctl disable php8.5-fpm
 
   update-alternatives --set php /usr/bin/php8.3
   update-alternatives --set php-config /usr/bin/php-config8.3
   update-alternatives --set phpize /usr/bin/phpize8.3
+  
+  systemctl enable php8.3-fpm
 
   # Install Composer
   curl -sS https://getcomposer.org/installer | php
@@ -754,7 +802,7 @@ fi
 if "$SKIP_POSTGRESQL"; then
   echo "SKIP_POSTGRESQL is being used, so we're not installing PostgreSQL"
 else
-  # Install Postgres 14
+  # Install Postgres 15
   apt-get install -y postgresql-15 postgresql-server-dev-15 postgresql-15-postgis-3 postgresql-15-postgis-3-scripts
 
   # Configure Postgres Users
